74309a8a93bdd6c13660afa49c0e51a1a90130d9
Riccardo Volonterio <voloxxx@gmail.com>
2016-10-10 16:16:25 +0200

    fix: Fixed source polluting, Added more params to API

--- src/index.ts
+++ src/index.ts
@@ -7,47 +7,60 @@ import * as initDebug from 'debug';
 // Load my modules
 
 // Constant declaration
-const debug = initDebug( 'stream-funnel' );
+const debug = initDebug('stream-funnel');
 
 // Module variables declaration
 
 // Module functions declaration
 
 // Module type declaration
+export interface FunnelOptions extends TransformOptions {
+  maxListeners?: number,
+  name?: string,
+}
 
 // Module class declaration
 export default class Funnel extends PassThrough {
-  private sources: number = 0;
+  private static id: number = 0;
+  private numSources: number = 0;
+  private name: string;
 
-  constructor( options: TransformOptions ) {
-    super( options );
-    // super( { objectMode: true } );
-    this.setMaxListeners( 0 );
+  constructor(options: FunnelOptions) {
+    super(options);
+
+    this.name = options.name || `Funnel(${++Funnel.id})`;
+    this.setMaxListeners(options.maxListeners || 0);
+    debug('Created: %s', this);
+  }
+  toString() {
+    return this.name;
   }
 
   // Methods
-  public addSource( source: NodeJS.ReadableStream ) {
-    this.sources += 1;
-    debug( 'Adding source: %d', this.sources );
+  public addSource(source: NodeJS.ReadableStream, waitForEnd: boolean = true) {
+    this.numSources += 1;
+    debug('%s: adding source, total %d', this, this.numSources);
 
-    source.on( 'end', () => this.sourceFinished( source ) );
-    source.pipe( this, { end: false } );
+    if (waitForEnd) {
+      source.once('end', () => this.sourceFinished(source));
+    }
+    source.pipe(this, { end: false });
   }
-  public addSources( sources: NodeJS.ReadableStream[] ) {
-    for( const source of sources ) {
-      this.addSource( source );
+  public addSources(sources: NodeJS.ReadableStream[], waitForEnd: boolean = true) {
+    for (const source of sources) {
+      this.addSource(source, waitForEnd);
     }
   }
 
-  public sourceFinished( source: NodeJS.ReadableStream ) {
-    this.sources -= 1;
-    debug( 'Source finished: %d', this.sources );
+  public sourceFinished(source: NodeJS.ReadableStream) {
+    this.numSources -= 1;
+    debug('%s: source finished, left %d', this, this.numSources);
 
     // Unpipe from self
-    source.unpipe( this );
+    source.unpipe(this);
 
-    if( this.sources === 0 ) {
-      debug( '%s, no more sources', this );
+    if (this.numSources === 0) {
+      debug('%s, no more sources', this);
       // No more account to wait for...
       this.end();
     }
